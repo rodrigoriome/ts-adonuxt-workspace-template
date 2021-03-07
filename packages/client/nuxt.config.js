de253854d5be410439e4ca9f66fb95f8b024f6d2
Rodrigo Mesquita <rodrigoriome@gmail.com>
2021-03-07 19:50:13 -0300

    Setup Typescript

--- packages/client/nuxt.config.js
+++ packages/client/nuxt.config.js
@@ -31,7 +31,11 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build'
   ],
+
+  modulesDir: ['../../node_modules'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -39,5 +43,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['@adonuxt']
   }
 }
---
+++ packages/server/app/Models/User.ts
@@ -0,0 +1,5 @@
+export default class User {
+  sayHello() {
+    console.log('Hello world')
+  }
+}
